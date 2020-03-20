93053b040f0f32d94de90e9229a92bfb0eb9a6bf
Vasco <vascomartins37@gmail.com>
2020-03-20 18:47:56 +0000

    fix: translation typo

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,5 +2,5 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('epoch') }}'><i class="nav-icon fa fa-calendar"></i> Épocas</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('day') }}'><i class='nav-icon fa fa-calendar-check-o'></i> Days</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('day') }}'><i class='nav-icon fa fa-calendar-check-o'></i> Dias</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('documentation') }}'><i class='nav-icon fa fa-file'></i> Documentação</a></li>
